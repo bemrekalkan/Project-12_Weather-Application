fdbc44307e354c97e83d56d56424cbedd21ec8d8
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-16 20:32:59 +0300

    .

--- weather.js
+++ weather.js
@@ -4,4 +4,7 @@ const input = document.querySelector(".top-banner input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
-localStorage.setItem("apiKey", "57f7704448070c6a2766738dc3a5fbbb");
+localStorage.setItem(
+  "apiKey",
+  EncryptStringAES(userTokenKey));
+);
