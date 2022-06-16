e823a7d368285f45f09c9756d890fa8aca9b1d7b
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-16 21:08:57 +0300

    api url added

--- weather.js
+++ weather.js
@@ -5,11 +5,17 @@ const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault(); //? page won't refresh after submitting
+  e.preventDefault(); //? page won't be refreshed after submitting
   getWeatherDataFormApi();
 });
 
 const getWeatherDataFormApi = () => {
-  alert("http request gone");
+  //   alert("http request gone");
+
+  let apiKey = DecryptStringAES(localStorage.getItem("apiKey"));
+  let inputValue = input.value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  `;
+
   form.reset(); //? same as ---> input.value = "";
 };
