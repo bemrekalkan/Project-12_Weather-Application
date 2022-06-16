2fb91f5b4868073b1d0d958c8c40f4544b2a1c7a
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-16 20:43:39 +0300

    addEventListener added to submit button

--- weather.js
+++ weather.js
@@ -3,3 +3,13 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //? page won't refresh after submitting
+  getWeatherDataFormApi();
+});
+
+const getWeatherDataFormApi = () => {
+  alert("http request gone");
+  form.reset(); //? same as ---> input.value = "";
+};
