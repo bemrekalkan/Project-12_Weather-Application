c1e0342ff0cdb3a4383a48959d22191f86c67650
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-16 20:18:51 +0300

    selectors added.

---
+++ weather.js
@@ -0,0 +1,5 @@
+//? SELECTORS
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector("span.msg");
+const list = document.querySelector(".ajax-section .cities");
