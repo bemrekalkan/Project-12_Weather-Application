0e9651438a51cc75c613b079019378f9ed0d8068
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-16 20:34:58 +0300

    apiKey is stored in Localstorage

--- weather.js
+++ weather.js
@@ -3,8 +3,3 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
-
-localStorage.setItem(
-  "apiKey",
-  EncryptStringAES(userTokenKey));
-);
