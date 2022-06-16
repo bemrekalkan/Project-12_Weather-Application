2f2f08b00b16b1b220f5817246c9935c1ae75c24
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-16 20:28:27 +0300

    apiKey added

--- weather.js
+++ weather.js
@@ -3,3 +3,5 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
+
+localStorage.setItem("apiKey", "57f7704448070c6a2766738dc3a5fbbb");
